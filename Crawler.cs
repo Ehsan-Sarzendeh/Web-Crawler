8774ceba661ef32d65cac36ee58651e4a53e4ed7
Ehsan Sarzendeh <ehsansarzendeh@gmail.com>
2023-11-16 14:08:22 +0330

    Update Crawler Regex

--- Crawler.cs
+++ Crawler.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.IO;
 using System.Text.RegularExpressions;
 
 namespace WebCrawler;
@@ -9,7 +8,12 @@ public class Crawler
     private readonly string _seedUrl;
     private readonly int _maxPageNo;
 
-    private readonly HashSet<string> _repository;
+    private int _fetchError;
+    private int _typeError;
+
+    private int _repositoryUrl;
+    private int _discoveredUrl;
+
     private readonly HashSet<string> _urlSet;
     private readonly Queue<string> _urlQueue;
     private HashSet<string> _disallowPath;
@@ -21,7 +25,6 @@ public class Crawler
         _seedUrl = seedUrl;
         _maxPageNo = maxPageNo;
         _disallowPath = new HashSet<string>();
-        _repository = new HashSet<string>();
         _urlSet = new HashSet<string> { seedUrl };
         _urlQueue = new Queue<string>();
         _urlQueue.Enqueue(seedUrl);
@@ -31,23 +34,23 @@ public class Crawler
     {
         var watch = Stopwatch.StartNew();
         _disallowPath = await ReadRobotsDisallowPath();
-        while (_urlQueue.Count > 0 && _repository.Count < _maxPageNo)
+        while (_urlQueue.Count > 0 && _repositoryUrl < _maxPageNo)
         {
             await Crawl();
-            Thread.Sleep(new TimeSpan(0, 0, 0, 0, 50));
         }
         watch.Stop();
 
-        Console.WriteLine($"Crawled Pages Count: {_repository.Count}");
-        Console.WriteLine($"Parsed Urls Count:   {_urlSet.Count}");
-        Console.WriteLine($"Queue Urls Count:    {_urlQueue.Count}");
         Console.WriteLine($"Total Time:          {watch.Elapsed.Hours}:{watch.Elapsed.Minutes}:{watch.Elapsed.Seconds}");
+        Console.WriteLine($"Queue Urls Count:    {_urlQueue.Count}");
+        Console.WriteLine($"Checked Urls Count:  {_urlSet.Count}");
+        Console.WriteLine($"Parsed Urls Count:   {_discoveredUrl}");
+        Console.WriteLine($"Fetch Error Count:   {_fetchError}");
+        Console.WriteLine($"Type Error Count:    {_typeError}");
     }
 
     private async Task Crawl()
     {
         var url = _urlQueue.Dequeue();
-        // Console.WriteLine(url);
         var htmlContent = await DownloadPage(url);
         if (!string.IsNullOrWhiteSpace(htmlContent))
         {
@@ -63,15 +66,22 @@ public class Crawler
             var htmlResponse = await _httpClient.GetAsync(url);
 
             if (htmlResponse is not { IsSuccessStatusCode: true })
+            {
+                _fetchError++;
                 return null;
+            }
 
             if (!htmlResponse.Content.Headers.ContentType!.MediaType!.Contains("text/html"))
+            {
+                _typeError++;
                 return null;
+            }
 
             return await htmlResponse.Content.ReadAsStringAsync();
         }
         catch (Exception)
         {
+            _fetchError++;
             return null;
         }
     }
@@ -84,17 +94,19 @@ public class Crawler
         var repPath = Directory.GetCurrentDirectory() + "\\Pages\\URLRep.txt";
         File.AppendAllText(repPath, url + "\n");
 
-        _repository.Add(url);
+        _repositoryUrl++;
     }
 
     public void ParseUrLs(string htmlContent)
     {
-        var regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+        var regexLink = new Regex("(?<=<a\\s+(?:[^>]*?\\s+)?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
         foreach (var match in regexLink.Matches(htmlContent))
         {
             try
             {
                 var link = match.ToString()!.Trim().Trim('\n');
+                _discoveredUrl++;
 
                 if (!IsLinkAcceptable(link))
                     continue;
@@ -125,7 +137,7 @@ public class Crawler
             if (string.IsNullOrEmpty(link) || string.IsNullOrWhiteSpace(link))
                 return false;
 
-            if (link.StartsWith("#"))
+            if (link.StartsWith("#") || link.StartsWith("$"))
                 return false;
 
             if (link.StartsWith("mailto:") || link.StartsWith("tel:") || link.StartsWith("sms:"))
@@ -145,8 +157,16 @@ public class Crawler
 
         bool IsLinkDisallow(string link)
         {
-            var linkUri = new Uri(link);
-            return _disallowPath.Any(x => linkUri.AbsolutePath.StartsWith(x));
+            try
+            {
+                var linkUri = new Uri(link);
+                return _disallowPath.Any(x => linkUri.AbsolutePath.StartsWith(x));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 
